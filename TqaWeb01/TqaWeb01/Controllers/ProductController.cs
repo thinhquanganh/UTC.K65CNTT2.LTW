8b1b101bc5d2299e63b5876c9149ff71cc013fbb
thinhquanganh0910206-design <thinhquanganh0910206@gmail.com>
2026-08-23 10:14:25 +0700

    TqaWeb01

---
+++ TqaWeb01/TqaWeb01/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TqaWeb01.Models;
+
+namespace TqaWeb01.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var listProducts = new List<Product>
+            {
+                new Product { Id = 1, Name = "Dép bông", Price = 49999, CreatedAt = new DateTime(2026, 8, 20), ImageUrl = "/images/depbongditrongnha.png" },
+                new Product { Id = 2, Name = "Dép đế chống trượt", Price = 79999, CreatedAt = new DateTime(2026, 8, 20), ImageUrl = "/images/depdechongtruot.png" },
+                new Product { Id = 3, Name = "Dép đi trong nhà", Price = 129999, CreatedAt = new DateTime(2026, 8, 20), ImageUrl = "/images/depditrongnha.png" },
+                new Product { Id = 4, Name = "Dép quai ngang", Price = 59999, CreatedAt = new DateTime(2026, 8, 20), ImageUrl = "/images/depquaingang.png" }
+            };
+            return View(listProducts);
+        }
+    }
+}
---
+++ TqaWeb01/TqaWeb01/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace TqaWeb01.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
