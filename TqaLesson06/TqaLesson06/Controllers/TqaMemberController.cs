2f4063ab49b0dbb35f9e4d0df6efbdc574b48c44
thinhquanganh0910206-design <thinhquanganh0910206@gmail.com>
2026-09-25 21:38:49 +0700

    Tqa lesson06 models demo

---
+++ TqaLesson06/TqaLesson06/Controllers/TqaMemberController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TqaLesson06.Models;
+
+namespace TqaLesson06.Controllers
+{
+    public class TqaMemberController : Controller
+    {
+        private static readonly List<TqaMember> _tqaMember = new List<TqaMember>()
+        {
+            new TqaMember
+    {
+        TqaMemberId = Guid.NewGuid().ToString(),
+        TqaMemberUserName = "quanganh206",
+        TqaMemberPassword = "quanganh",
+        TqaMemberEmail = "thinquanganh0910206@gmail.com",
+        TqaMemberFullName = "Thịnh Quang Anh"
+    },
+
+    new TqaMember
+    {
+        TqaMemberId = Guid.NewGuid().ToString(),
+        TqaMemberUserName = "tranthib",
+        TqaMemberPassword = "123456",
+        TqaMemberEmail = "tranthib@gmail.com",
+        TqaMemberFullName = "Trần Thị B"
+    },
+
+    new TqaMember
+    {
+        TqaMemberId = Guid.NewGuid().ToString(),
+        TqaMemberUserName = "levanc",
+        TqaMemberPassword = "123456",
+        TqaMemberEmail = "levanc@gmail.com",
+        TqaMemberFullName = "Lê Văn C"
+    }
+        };
+        public IActionResult TqaIndex()
+        {
+            return View(_tqaMember);
+        }
+        public IActionResult TqaCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult TqaCreate(TqaMember tqaMember)
+        {
+            tqaMember.TqaMemberId = Guid.NewGuid().ToString();
+            _tqaMember.Add(tqaMember);
+            return RedirectToAction("TqaIndex");
+        }
+        public IActionResult TqaEdit(string id)
+        {
+            var tqaMember = _tqaMember.FirstOrDefault(x => x.TqaMemberId.Equals(id));
+            return View(tqaMember);
+        }
+        [HttpPost]
+        public IActionResult TqaEdit( string id ,TqaMember tqaMember)
+        {
+            for(int i = 0; i < _tqaMember.Count; i++)
+            {
+                if (_tqaMember[i].TqaMemberId == id)
+                {
+                    _tqaMember[i].TqaMemberId = tqaMember.TqaMemberId;
+                    _tqaMember[i].TqaMemberUserName = tqaMember.TqaMemberUserName;
+                    _tqaMember[i].TqaMemberPassword = tqaMember.TqaMemberPassword;
+                    _tqaMember[i].TqaMemberFullName = tqaMember.TqaMemberFullName;
+                    _tqaMember[i].TqaMemberEmail = tqaMember.TqaMemberEmail;
+                    break;
+                }
+                
+            }
+            return RedirectToAction("TqaIndex");
+        }
+
+        public IActionResult TqaGetDetails()
+        {
+            var tqaMember = new TqaMember()
+            {
+                TqaMemberId = Guid.NewGuid().ToString(),
+                TqaMemberUserName = "Quang Anh",
+                TqaMemberPassword = "quanganh",
+                TqaMemberFullName = "Thịnh Quang Anh",
+                TqaMemberEmail = "thinhquanganh0910206@gmail.com"
+            };
+            return View(tqaMember);
+        }
+    }
+}
---
+++ TqaLesson06/TqaLesson06/Models/TqaMember.cs
@@ -0,0 +1,12 @@
+﻿namespace TqaLesson06.Models
+{
+    public class TqaMember
+    {
+        public string TqaMemberId { get; set; }
+        public string TqaMemberUserName { get; set; }
+        public string TqaMemberPassword { get; set; }
+        public string TqaMemberEmail { get; set; }
+        public string TqaMemberFullName { get; set; }
+
+    }
+}
